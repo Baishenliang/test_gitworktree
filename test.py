8aa1765fbdb7bcbf614cae771eb1ad385535cf6c
Baishenliang <liangbs95@gmail.com>
2024-11-24 17:42:19 -0500

    updated file

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-from worktree_global.test_para import print_text\n\nprint_text()
+import sys
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+target_dir = os.path.join(current_dir, '../worktree-global')
+sys.path.append(target_dir)
+
+from test_para import print_text
+print_text()
